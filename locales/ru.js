cc48c58f936bc24a93d7060c6f5cfc779e083276
Ilya <i1ya1l@yandex.ru>
2021-05-08 23:22:36 +0300

    step-3

---
+++ locales/ru.js
@@ -0,0 +1,17 @@
+export default {
+  translation: {
+    form: {
+      errors: {
+        inputValidUrl: 'Ссылка должна быть валидным URL',
+        rssAlreadyExists: 'RSS уже существует',
+      },
+    },
+    app: {
+      rssAdded: 'RSS успешно загружен',
+      rssError: 'Ресурс не содержит валидный RSS',
+      errors: {
+        feedHasntBeenUpdated: 'Ошибка при обновлении RSS ленты: {{feedName}}. Ошибка: {{origError}}',
+      },
+    },
+  },
+};
